e5c048cc3c8d0382e79ba1cd931f1781b23316ef
Bryan McClellan <mccbryan.ops@gmail.com>
2023-01-21 08:03:05 -0600

    intial commit

---
+++ network.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "jenkins_demo" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+
+resource "azurerm_virtual_network" "jenkins_demo" {
+  name                = "demo-network"
+  location            = azurerm_resource_group.jenkins_demo.location
+  resource_group_name = azurerm_resource_group.jenkins_demo.name
+  address_space       = ["10.150.0.0/16"]
+
+  subnet {
+    name           = var.subnet_name
+    address_prefix = "10.150.1.0/24"
+  }
+
+  tags = var.tags
+}
+
+data "azurerm_subnet" "jenkins_demo" {
+  name = var.subnet_name
+  resource_group_name = azurerm_resource_group.jenkins_demo.name
+  virtual_network_name = azurerm_virtual_network.jenkins_demo.name
+  depends_on = [
+    azurerm_virtual_network.jenkins_demo
+  ]
+}
+
---
+++ vars.tf
@@ -0,0 +1,28 @@
+variable "location" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
+
+variable "tags" {
+  type = map(any)
+}
+
+variable "admin_user" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+  
+}
